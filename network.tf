9c10fe2d2a99ca1ac8c2902a3203819f1b506635
Drew Holt <drewderivative@gmail.com>
2021-07-22 11:36:38 -0600

    change to STANDARD network tier

--- network.tf
+++ network.tf
@@ -4,8 +4,14 @@ resource "google_compute_network" "default" {
   #auto_create_subnetworks = "false"
 }
 
+resource "google_compute_project_default_network_tier" "default" {
+  network_tier = "STANDARD"
+}
+
 resource "google_compute_address" "invadelabs-ext" {
   name = "invadelabs-ext"
+  description = "static external ip for invadelabs.com"
+  network_tier = "STANDARD"
 }
 
 resource "google_compute_firewall" "http-server" {
