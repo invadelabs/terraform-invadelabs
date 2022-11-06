d853d14422949304e58b06c280856d92e43cc5dc
Drew Holt <17417299+drew-holt@users.noreply.github.com>
2022-11-05 19:11:56 -0600

    update to latest cloudflare / gcp providers and latest terraform (#18)

--- gcp/main-blue.tf
+++ gcp/main-blue.tf
@@ -1,6 +1,6 @@
-#resource "google_compute_instance" "invadelabs" {
+#resource "google_compute_instance" "invadelabs_blue" {
 #  name         = "invadelabs-blue"
-#  description  = "invadelabs.com gcp vm"
+#  description  = "invadelabs.com gcp vm (blue)"
 #  machine_type = "e2-micro"
 #  zone         = "us-west1-a"
 #  hostname     = "invadelabs.com"
@@ -10,13 +10,14 @@
 #    "https-server",
 #    "allow-drew-nm1",
 #  ]
+#  # runs each boot, not first boot only
 #  #metadata_startup_script = "sudo apt-get update && sudo apt-get install -y ansible rclone etckeeper && cd /etc && etckeeper init"
-#  deletion_protection     = true
+#  deletion_protection = true
 #
 #  boot_disk {
-#    device_name = "invadelabs-disk-0"
+#    device_name = "invadelabs-blue-disk-0"
 #    initialize_params {
-#      image = "ubuntu-2004-focal-v20210720"
+#      image = "ubuntu-2204-lts"
 #      #labels = { "key3" : "value3" }
 #      type = "pd-standard" # or pd-balanced or pd-ssd
 #      size = "10"
@@ -29,10 +30,10 @@
 #  }
 #
 #  network_interface {
-#    # A default network is created for all GCP projects
-#    network = google_compute_network.invadelabs.id
-#    #access_config {
+#    subnetwork = "invadelabs"
+#    # do not uncomment access_config {} block to get ephemeral IP
+#    access_config {
 #      # nat_ip = google_compute_address.invadelabs-ext.address # un/comment to associate main IP
-#    #}
+#    }
 #  }
 #}
